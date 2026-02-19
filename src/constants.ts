bc4d77654dfb1b362c650ab63bb325ce122217bd
Claire Froelich <claire.froelich@gmail.com>
2026-02-19 14:44:39 -0800

    higher fidelity logo

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // TODO: .env
 const constants = {
-  CONTENTFUL_SPACE_ID: "dzohsh5fpsgf",
-  CONTENTFUL_ACCESS_TOKEN: "mrMwxge9oPPyt2P3_ieg5mTa-hwC9kOoKHyFM1E519Q",
+  CONTENTFUL_SPACE_ID: import.meta.env.CONTENTFUL_SPACE_ID,
+  CONTENTFUL_ACCESS_TOKEN: import.meta.env.CONTENTFUL_ACCESS_TOKEN,
 };
 
 export default constants;
